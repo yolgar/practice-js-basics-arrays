68adf47ee4c5addcc5a9cb93d208d27d48f70876
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-05-27 12:45:08 +0200

    Finish .length

---
+++ 01/app.js
@@ -0,0 +1,4 @@
+const users = ['Anna Anitowa', 'Beata Beatowska', 'Cycylia Cycylśka', 'Daria Darkowska', 'Elisa Elisowska'];
+
+console.log(users[0], users[2], users[4]);
+console.log(users.length);
