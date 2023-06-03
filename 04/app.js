b1feaccdf58851bf215b6b2ceb2eb2227645fa7b
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-06-03 11:32:51 +0200

    new Date & map

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,15 @@
+// solution 1
 const years = [1980, 1934, 2002, 2019];
+
+const currYear = (new Date()).getFullYear();
+const differenceTable = years.map(x => currYear - x);
+console.log(differenceTable);
+
+//solution 2
+// const years = [1980, 1934, 2002, 2019];
+
+// const currYear = (new Date()).getFullYear();
+// const differenceTable = years.map(function(item){
+// return currYear - item;
+// });
+// console.log(differenceTable);
